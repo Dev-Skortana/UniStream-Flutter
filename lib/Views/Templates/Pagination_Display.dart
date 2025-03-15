d15115b7c5bb692b22273f646232d3bf6e186f79
Dev-Skor <abokicontactpro@gmail.com>
2025-03-15 01:01:02 +0100

    Creation du système de pagination pour les interfaces de consultations

--- lib/Views/Templates/Pagination_Display.dart
+++ lib/Views/Templates/Pagination_Display.dart
@@ -1,8 +1,21 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:unistream/ViewModels/Templates/ViewModel_VideoBase.dart';
 
 class PaginationDisplay extends StatefulWidget {
+  late ViewmodelVideobase viewmodelVideo;
+  late ValueNotifier videoNotifier;
+
+  PaginationDisplay(
+      {Key? key,
+      required ViewmodelVideobase viewmodel_Video,
+      required ValueNotifier video_notifier})
+      : super(key: key) {
+    this.viewmodelVideo = viewmodel_Video;
+    this.videoNotifier = video_notifier;
+  }
+
   @override
   State<StatefulWidget> createState() => PaginationDisplayState();
 }
@@ -20,23 +33,27 @@ class PaginationDisplayState extends State<PaginationDisplay> {
     this.controlTotalFiche = GlobalKey();
   }
 
-  void valueChanged({required int indexvideo, required int index}) {
-    /* */
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Fiche : "),
-          Text(key: this.controlNumberFiche, ""),
-          Text("/"),
-          Text(key: this.controlTotalFiche, "Total")
-        ],
-      ),
+      child: ValueListenableBuilder(
+          valueListenable: widget.videoNotifier,
+          builder: (context, value, child) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Fiche : "),
+                Text(
+                    key: this.controlNumberFiche,
+                    (widget.viewmodelVideo.index + 1).toString()),
+                Text("/"),
+                Text(
+                    key: this.controlTotalFiche,
+                    widget.viewmodelVideo.TotalCount.toString())
+              ],
+            );
+          }),
     );
   }
 }
