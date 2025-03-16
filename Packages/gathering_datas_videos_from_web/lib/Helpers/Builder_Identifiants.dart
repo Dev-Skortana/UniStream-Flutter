8531b3ea9e4c4c458852d24ee204843d9ba6a915
Dev-Skor <abokicontactpro@gmail.com>
2025-03-16 08:27:45 +0100

    Création du generateur d'identifiants pour le fichier json register.

--- Packages/gathering_datas_videos_from_web/lib/Helpers/Builder_Identifiants.dart
+++ Packages/gathering_datas_videos_from_web/lib/Helpers/Builder_Identifiants.dart
@@ -1,12 +1,17 @@
 part of gathering_data_videos_from_web;
 
 class BuilderIdentifiants {
+  static const CHARS =
+      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+
   BuilderIdentifiants() {}
 
   List<String> getIdentifiants(
-      {required int how_many_identifiers, required int lenght}) {
-    return [];
-  }
+          {required int how_many_identifiers, int length = 12}) =>
+      List.generate(
+          how_many_identifiers, (int index) => this._CreateIdentifiant(length));
 
-  String _CreateIdentifiant() => "";
+  String _CreateIdentifiant(int length) =>
+      String.fromCharCodes(Iterable.generate(
+          length, (_) => CHARS.codeUnitAt(Random().nextInt(CHARS.length))));
 }
